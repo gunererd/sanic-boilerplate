dff2b754990d9359d0c23513dd7d9b4fa8813b38
gunererd <gunererd@gmail.com>
2019-11-26 10:27:42 +0300

    fix

--- test/conftest.py
+++ test/conftest.py
@@ -12,13 +12,10 @@ from src.utils.general import load_settings_from_environments
 @pytest.fixture(scope='session')
 def settings():
 
-    dotenv_filepath = Path('.')
+    dotenv_filepath = Path('.') / '.env'
     _settings = load_settings_from_environments(dotenv_path=dotenv_filepath, verbose=False)
 
     yield _settings
-
-    os.remove(dotenv_filepath)
-
 
 @pytest.fixture(scope='session')
 def app(settings):
