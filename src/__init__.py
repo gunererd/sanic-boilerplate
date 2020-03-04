772bed428c4f9d183a0ef6303cfac94ed0588252
gunererd <gunererd@gmail.com>
2020-03-04 13:05:44 +0300

    initialize di container on app creation

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,13 @@
 from sanic import Sanic
 
+from src.utils.di import DIContainer
+
 
 def create_app(settings):
     app = Sanic(__name__)
     app.settings = settings
+
+    app.di = DIContainer()
 
     from .services import init_services
     app.register_listener(init_services, 'before_server_start')
