f40ab5ecc07a223a3acac885d2a7acc2b3ff34a7
gunererd <gunererd@gmail.com>
2020-03-04 13:53:51 +0300

    add save method to mongodb

--- src/services/databases/mongodb.py
+++ src/services/databases/mongodb.py
@@ -1,12 +1,26 @@
 import logging
 
+from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.errors import OperationFailure
+from pymongo.errors import OperationFailure, DuplicateKeyError
 
 from src.utils import errors
 
-
 logger = logging.Logger('MongoDB')
+
+
+def maybe_object_id(maybe_id):
+    if isinstance(maybe_id, ObjectId):
+        return maybe_id
+    elif not ObjectId.is_valid(maybe_id):
+        return maybe_id
+    else:
+        return ObjectId(maybe_id)
+
+
+def create_id():
+    return ObjectId()
+
 
 class MongoDb(object):
 
@@ -52,3 +66,33 @@ class MongoDb(object):
                 )
 
             raise
+
+    async def save(self, document, collection_name, force=False):
+        collection = self.db[collection_name]
+
+        if "_id" not in document:
+            document["_id"] = create_id()
+        else:
+            document['_id'] = maybe_object_id(document['_id'])
+        try:
+            if force:
+                await collection.replace_one(
+                    {'_id': document['_id']},
+                    document,
+                    upsert=True
+                )
+            else:
+                await collection.insert_one(document)
+
+        except DuplicateKeyError as e:
+            raise errors.Error(
+                err_code="errors.duplicateKeyError",
+                status=409,
+                err_msg="There is already existing record in collection<{}>.".format(collection_name),
+                context={
+                    'collection': collection_name,
+                    'error_message': e.details['errmsg']
+                }
+            )
+
+        return document
